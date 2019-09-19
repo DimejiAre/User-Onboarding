56bd30caae644398a99ebc36e9ed801ccd80b384
DimejiAre <olardi2@gmail.com>
2019-09-19 13:19:45 +0100

    set up validation for fields using yup

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as yup from 'yup';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const initialUser = {
@@ -9,9 +10,20 @@ const onSubmit = () => {
 
 }
 
+const validationSchema = yup.object().shape({
+    name: yup.string()
+    .required('You must enter a Name'),
+    email: yup.string()
+    .email('You must enter a valid email address')
+    .required('You must enter a valid email address'),
+    password: yup.string()
+    .required('You must enter a password'),
+  });
+
 function UserForm(props){
     return (
         <Formik 
+        validationSchema={validationSchema}
         initialValues={initialUser}
         onSubmit={onSubmit}
         render={props => {
@@ -20,18 +32,22 @@ function UserForm(props){
                     <label>
                         Name
                         <Field name='name' type='text' placeholder='Name'/>
+                        <ErrorMessage name='name' component='div' />
                     </label>
                     <label>
                         Email
                         <Field name='email' type='text' placeholder='Email'/>
+                        <ErrorMessage name='name' component='div' />
                     </label>                    
                     <label>
                         Password
                         <Field name='password' type='password' placeholder='Password'/>
+                        <ErrorMessage name='name' component='div' />
                     </label>                   
                     <label>
                         I agree to terms and conditions
                         <Field name='terms' type='checkbox' />
+                        <ErrorMessage name='name' component='div' />
                     </label>                   
                     <button type='submit'>Submit</button>
                 </Form>
